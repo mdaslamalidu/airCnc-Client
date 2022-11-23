60fd1f21d1798b7271b39babf026868215652f81
mdaslamalidu <thehometutor2022@gmail.com>
2022-11-23 08:53:32 +0600

    add user api

--- src/api/user.js
+++ src/api/user.js
@@ -1,5 +1,5 @@
 export const hostDataApi = async (host) => {
-  const url = `http://localhost:8000/user/${host?.email}`;
+  const url = `http://localhost:8000/users/${host?.email}`;
 
   const response = await fetch(url, {
     method: "PUT",
@@ -10,5 +10,16 @@ export const hostDataApi = async (host) => {
   });
 
   const data = await response.json();
+  console.log(data);
+  return data;
+};
+
+export const getuser = async (email) => {
+  const url = `http://localhost:8000/users/${email}`;
+
+  const response = await fetch(url);
+
+  const data = await response.json();
+  console.log(data);
   return data;
 };
